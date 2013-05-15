5c52e68cb415c678bb6af9eb68faf753381b82a5
Hovhannes Hambardzumyan <hovhanneshambardzumyan@gmail.com>
2013-05-15 17:06:20 +0400

    Start Test

---
+++ src/com/Letsein/TimeRaven/NewActivity.java
@@ -0,0 +1,18 @@
+package com.Letsein.TimeRaven;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: studenta
+ * Date: 5/15/13
+ * Time: 3:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NewActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_activity);
+    }
+}
